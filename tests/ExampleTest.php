ccbd7b00994c1a80e56cf32042e8ce6f36eb0458
Dani Roman <roman.martinez.daniel@gmail.com>
2018-11-09 01:25:48 +0100

    Just testing check name github

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class ExampleTest extends TestCase
 {
 
-    use DatabaseTransactions;
+    use DatabaseTransactions; 
 
     /**
      * A basic functional test example.
